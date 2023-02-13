bc485823dbb66f1d5cccf90ab551774cd72dc46a
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-13 20:55:39 +0530

    1. Commit

---
+++ JavaScriptFiles/2_variables.js
@@ -0,0 +1,6 @@
+var first_name = 'Priyam';
+var last_name = 'Sahu';
+console.log('Full Name: ' + first_name + ' ' + last_name); //Full Name: Priyam Sahu
+
+//Template Literals
+console.log(`Hi ${first_name} ${last_name}`); //Hi Priyam Sahu
