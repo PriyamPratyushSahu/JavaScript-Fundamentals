827b02b7c5ac3ccb7898b41e4754e9953b72aee3
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-13 20:45:14 +0530

    1. Commit

--- JavaScriptFiles/1_console.js
+++ JavaScriptFiles/1_console.js
@@ -1,1 +1,24 @@
-console.log('Hello World'); //Hello World
+console.log('Hello World'); //Hello 
+console.log(123); //123
+console.log(true);
+
+var greetings = 'Hi Everyone';
+console.log(greetings)
+
+//Array
+console.log([1, 2, 3, 4]);
+//Objects
+console.log({ a: 1, b: 2 })
+console.table({ a: 1, b: 2 })
+
+//To show error
+console.error("! ! ! !This is an error ! ! ! !");
+
+//To show warning
+console.warn("This is a warning")
+//To clear the console
+//console.clear()
+
+//Time
+console.time('Hello') //Start time
+console.timeEnd('Hello') //End time
