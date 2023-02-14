abb8885774bec5222c59239189ccadd8eacff149
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-14 13:02:56 +0530

    6_Numbers

---
+++ JavaScriptFiles/7_Operators.js
@@ -0,0 +1,8 @@
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
