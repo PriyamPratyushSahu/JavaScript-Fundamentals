573197f51acaf514ffa2dae6561b9be7d20e0f23
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-19 23:30:34 +0530

    Practice

--- JavaScriptFiles/practice.js
+++ JavaScriptFiles/practice.js
@@ -3,8 +3,13 @@ console.log('Practice File')
 
 //arrow function
 const avr = (a, b, c) => {
-    console.log("Average of 3 numbers:")
 return(Math.trunc((a + b + c)/3))
 }
+console.log(`Average of 3 numbers: ${avr(3, 6, 10)}` ) //Average of 3 numbers: 6
 
-console.log(avr(3, 6, 10));
+const func1 = (num = 10) => {
+    return num;
+}
+console.log(func1(10)); //10
+console.log(func1()); //10
+console.log(func1(30)); // 30
