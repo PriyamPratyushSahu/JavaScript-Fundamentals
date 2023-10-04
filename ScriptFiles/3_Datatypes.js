7ac47e0acd46330c7fbbb25467bea1ecb74912ba
unknown <priyampratyushsahu3232@gmail.com>
2023-10-04 11:04:10 +0530

    'Datatypes'

--- ScriptFiles/3_Datatypes.js
+++ ScriptFiles/3_Datatypes.js
@@ -8,27 +8,27 @@
 
 //String
 const fullName = 'Priyam Pratyush Sahu'
-console.log(fullName) //Priyam Pratyush Sahu
-console.log(typeof (fullName)) //string
+console.log(`fullName: ${fullName}`) //fullName: Priyam Pratyush Sahu
+console.log(`typeof (fullName): ${typeof (fullName)}`) //typeof (fullName): string
 
 //Number
 const age = 23
-console.log(age) //23
-console.log(typeof (age)) //number
+console.log(`age: ${age}`) //age: 23
+console.log(`typeof (age): ${typeof (age)}`) //typeof (age): number
 
 //Boolean
 const isMarried = false
-console.log(isMarried) //false
-console.log(typeof (isMarried)) //boolean
+console.log(`isMarried: ${isMarried}`) //isMarried: false
+console.log(`typeof (isMarried): ${typeof (isMarried)}`) //typeof (isMarried): boolean
 
 //Null
-console.log(typeof (null)) //object
+console.log(`typeof (null): ${typeof (null)}`) //typeof (null): object
 
 //Undefined
 let a
-console.log(a) //undefined
-console.log(typeof (a)) //undefined
-console.log(typeof (undefined)) //undefined
+console.log(`a: ${a}`) //a: undefined
+console.log(`typeof (a): ${typeof (a)}`) //typeof (a): undefined
+console.log(`typeof (undefined): ${typeof (undefined)}`) //typeof (undefined): undefined
 
 //Symbol
 const sym = Symbol();
@@ -38,8 +38,8 @@ console.log(sym) //Symbol()
 
 //Array
 const hobbies = ['reading', 'music', 'cooking']
-console.log(hobbies) // ['reading', 'music', 'cooking']
-console.log(typeof (hobbies)) //object
+console.log(`hobbies: ${hobbies}`) //hobbies: reading,music,cooking
+console.log(`typeof (hobbies): ${typeof (hobbies)}`) //typeof (hobbies): object
 
 //Object Literal
 const address = {
@@ -47,23 +47,32 @@ const address = {
     country: 'India'
 }
 console.log(address) //{state: 'Odisha', country: 'India'}
-console.log(typeof (address)) //object
+console.log(`typeof (address): ${typeof (address)}`) //typeof (address): object
 
 //Dynamically Typed
 
 let dynamicVar
 
 dynamicVar = 1;
-console.log(typeof (dynamicVar)) //number
-
+console.log(`dynamicVar: ${dynamicVar}\ntypeof (dynamicVar): ${typeof (dynamicVar)}`)
+//dynamicVar: 1
+//typeof (dynamicVar): number
 dynamicVar = "Hi"
-console.log(typeof (dynamicVar)) //string
+console.log(`dynamicVar: ${dynamicVar}\ntypeof (dynamicVar): ${typeof (dynamicVar)}`)
+//dynamicVar: Hi
+//typeof (dynamicVar): string
 
 dynamicVar = true
-console.log(typeof (dynamicVar)) //boolean
+console.log(`dynamicVar: ${dynamicVar}\ntypeof (dynamicVar): ${typeof (dynamicVar)}`)
+//dynamicVar: true
+//typeof (dynamicVar): boolean
 
 dynamicVar = null
-console.log(typeof (dynamicVar)) //object
+console.log(`dynamicVar: ${dynamicVar}\ntypeof (dynamicVar): ${typeof (dynamicVar)}`)
+//dynamicVar: null
+//typeof (dynamicVar): object
 
 dynamicVar = [1, 2, 3, 4]
-console.log(typeof (dynamicVar)) //object
+console.log(`dynamicVar: ${dynamicVar}\ntypeof (dynamicVar): ${typeof (dynamicVar)}`)
+//dynamicVar: 1,2,3,4
+//typeof (dynamicVar): object
